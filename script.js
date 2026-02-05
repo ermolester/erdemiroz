a1b046566c033d10c1294f06725520e7cbf55673
ermolester <istem7ler@gmail.com>
2026-02-05 16:20:43 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Basit kaydırma animasyonu
+document.querySelectorAll("a[href^='#']").forEach(link => {
+    link.addEventListener("click", e => {
+        e.preventDefault();
+        document.querySelector(link.getAttribute("href"))
+            .scrollIntoView({ behavior: "smooth" });
+    });
+});
